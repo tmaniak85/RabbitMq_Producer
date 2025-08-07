a7e6fc07e1dc80f779f7f9757b047f703ab319db
tmaniak85 <tomasz.maniak.85@gmail.com>
2025-08-07 18:45:06 +0200

    2.Consumer Is Real Time Indeed (20)

--- src/main/java/com/course/rabbitmq/producer/Application.java
+++ src/main/java/com/course/rabbitmq/producer/Application.java
@@ -5,9 +5,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 import java.util.concurrent.ThreadLocalRandom;
 
+@EnableScheduling
 @SpringBootApplication
 public class Application implements CommandLineRunner {
 
